1465f387e3789f7e29c38b80392c6de038ce86a3
Deepak Verma <deepak.23.verma@gmail.com>
2014-02-27 15:26:14 -0500

    Twitter remote sign-in working still require email and name to complete sign-in with twitter

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,16 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  def twitter
+  def provider
     user = User.process_omniauth(request.env["omniauth.auth"])
+    if user.persisted?
+      flash.notice = "Signed in!"
+      sign_in_and_redirect user
+    else
+      session["devise.user_attributes"] = user.attributes
+      redirect_to new_user_registration_url
+    end
   end
-  
+
+  alias_method :twitter, :provider
+
 end
